d886278c58d7ba4f673a5a85fc9deb3fe63ae3c6
AxxiB <axxibot@seznam.cz>
2016-08-29 03:13:01 +0200

    Update BOT.js

--- BOT.js
+++ BOT.js
@@ -33,7 +33,7 @@
             if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
             if (!bot.commands.executable(this.rank, chat)) return void (0);
             else {
-              API.sendChat("/me @" + user + " Majkův fanoušek šel AFK!!!");
+              API.sendChat("/me @" + user + "šel AFK!!!");
             }
           }
         };
@@ -45,7 +45,7 @@
             if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
             if (!bot.commands.executable(this.rank, chat)) return void (0);
             else {
-              API.sendChat("/me @" + user + " Majkův fanoušek se vrátil!!!");
+              API.sendChat("/me @" + user + "Se vrátil!!!");
             }
           }
         };
